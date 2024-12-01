78d81eb388066c0e0681b35d152bee7068dff158
Ruchi <ruchi.thanabhawan@gmail.com>
2024-12-01 15:33:36 -0800

    implement GET /posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,23 +1,30 @@
+import "dotenv/config";
 import express from "express";
+import Post from "../models/post.js";
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  res.send("Fetch all posts");
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
-router.get("/:id", function (req, res) {
+router.get("/:id", (req, res) => {
   res.send("Fetch post by id");
 });
 
-router.post("/", function (req, res) {
+router.post("/", (req, res) => {
   res.send("Create new post");
 });
 
-router.put("/:id", function (req, res) {
+router.put("/:id", (req, res) => {
   res.send("Update post by id");
 });
 
-router.delete("/:id", function (req, res) {
+router.delete("/:id", (req, res) => {
   res.send("Delete post by id");
 });
 
