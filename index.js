cdd43863db9c61e09e409c6b96c25b1738e9df1d
Ruchi <ruchi.thanabhawan@gmail.com>
2024-12-02 16:53:25 -0800

    install cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import cors from "cors";
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.error("Failed to connect to DB:", err));
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 const port = 3000;
 
